211f591e967dc4b8634d2a9ce1fc332fbf2b6639
Albert481 <albertyu481@gmail.com>
2021-06-10 23:15:52 +0800

    scenemanagement

---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project.Networking;
+using UnitySocketIO;
+using UnitySocketIO.Events;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Managers
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [Header("Join Now")]
+        [SerializeField]
+        private GameObject joinContainer;
+
+        [SerializeField]
+        private Button queueButton;
+
+
+        [Header("Sign In")]
+        [SerializeField]
+        private GameObject signInContainer;
+
+        private string username;
+        private string password;
+
+        public NetworkClient socketReference;
+
+        public NetworkClient SocketReference
+        {
+            get
+            {
+                return socketReference = (socketReference == null) ? FindObjectOfType<NetworkClient>() : socketReference;
+            }
+        }
+
+        public void Start()
+        {
+            queueButton.interactable = false;
+            signInContainer.SetActive(false);
+            joinContainer.SetActive(false);
+            
+            
+        }
+
+        public void OnQueue()
+        {
+            
+        }
+
+        public void OnSignIn()
+        {
+
+        }
+
+        public void OnSignInComplete()
+        {
+
+        }
+
+        public void OnCreateAccount()
+        {
+
+        }
+
+        public void EditUsername(string text)
+        {
+            username = text;
+        }
+
+        public void EditPassword(string text)
+        {
+            password = text;
+        }
+
+    }
+
+}
